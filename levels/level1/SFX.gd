e59aa3a2fc530fa05131a67e88fd10b7b3670e4c
gljubojevic <gljubojevic@users.noreply.github.com>
2020-04-19 14:39:33 +0200

    Add sfx for feedme and I'm hungry

--- levels/level1/SFX.gd
+++ levels/level1/SFX.gd
@@ -3,24 +3,26 @@ extends Node
 class_name SoundFX
 
 export (float) var playerWalkFreq = 0.3
-
-var _playerWalking:bool
-var _playerWalkingTime:float
+export (float) var feedSoundsFreq = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_playerWalking = false
 	_playerWalkingTime = 0
+	_nextFeedSnd = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_playerWalkingPlay(delta)
-	#_eatCow(delta)
+	_eatCowPlay(delta)
+	_rndFeed(delta)
+
+var _playerWalking:bool
+var _playerWalkingTime:float
 
 func playerWalking(status:bool):
 	if _playerWalking == status:
 		return
-	#print_debug("Player walking:", status)
 	_playerWalking = status
 	$WalkStep.stop()
 	if _playerWalking:
@@ -34,6 +36,37 @@ func _playerWalkingPlay(delta):
 	_playerWalkingTime += delta
 	if _playerWalkingTime > playerWalkFreq:
 		_playerWalkingTime = 0
+	
+var _eatCow:bool
+var _eatCowTime:float
 
-func _eatCow(delta):
-	pass
+func eatCow():
+	$DeadCow.stop()
+	$Eating.stop()
+	_eatCow = true
+	_eatCowTime = 0
+	$DeadCow.play()
+
+func _eatCowPlay(delta):
+	if !_eatCow:
+		return
+	if _eatCowTime > 0.5 && !$Eating.playing:
+		$Eating.play()
+	if _eatCowTime > 0.6 && !$Eating.playing:
+		_eatCow = false
+	_eatCowTime+=delta
+
+var _nextFeedSnd:float
+
+func _rndFeed(delta):
+	if _nextFeedSnd > 0:
+		_nextFeedSnd -= delta
+		if _nextFeedSnd < 0:
+			_nextFeedSnd=0
+		return
+
+	_nextFeedSnd = feedSoundsFreq + rand_range(0, feedSoundsFreq / 2)
+	if int(_nextFeedSnd) % 2 == 1:
+		$FeedMee.play()
+	else:
+		$ImHungry.play()
